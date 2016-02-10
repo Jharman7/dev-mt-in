04604720681184b5580180b48c9005e7d7b08c7b
Jeff Harman <jrharman7@gmail.com>
2016-02-09 19:50:08 -0700

    Finished day 1

---
+++ js/homeCtrl.js
@@ -0,0 +1,16 @@
+var app = angular.module('devMtIn');
+app.controller('homeCtrl', function ($scope) {
+  $scope.myProfile = {
+    name: 'John Doe'
+  , friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
+  }
+
+  $scope.sortOptions = [{
+    display: 'Ascending'
+  , value: false
+  },
+  {
+    display: 'Descending'
+  , value: true
+  }];
+});
